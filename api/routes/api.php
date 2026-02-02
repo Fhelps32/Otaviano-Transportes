8622592ee16f6a14ef45683ba19dac9a31add675
Epllef <lyipe32@gmail.com>
2026-02-02 12:36:55 -0300

    fazendo merge

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/enviar', [EmailController::class, 'testeEmail']);
+Route::get('/enviar', [EmailController::class, 'send']);
