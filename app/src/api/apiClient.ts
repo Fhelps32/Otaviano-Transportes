ff882320407e7540b501bb2b7128a4e31334930d
Epllef <lyipe32@gmail.com>
2026-03-01 14:06:42 -0300

    fix(apiClient): arrumar endereçamento de ip da api

--- app/src/api/apiClient.ts
+++ app/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://9.223.176.58/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
