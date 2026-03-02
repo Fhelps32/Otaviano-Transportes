60d765a69e9974a16371ff2169ff33c080cadf67
EuKauatf <kauafvr1612@gmail.com>
2026-03-02 00:08:09 -0300

    fix(apiClient): arrumar rota de url

--- app/src/api/apiClient.ts
+++ app/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://9.223.176.58/api",
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
